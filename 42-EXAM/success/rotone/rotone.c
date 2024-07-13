6717f43b9ceb1be763516072eb3d4123e027d2c6
ahaded777 <131011542+ahaded777@users.noreply.github.com>
2024-07-13 08:38:12 +0100

    Add files via upload

---
+++ 42-EXAM/success/rotone/rotone.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int main(int ac, char **av)
+{
+	if(ac != 2)
+	{
+		write(1, "\n", 1);
+		return 0;
+	}
+
+	int i;
+
+	i = 0;
+	while(av[1][i] != '\0')
+	{
+		if(av[1][i] >= 'a' && av[1][i] <= 'y')
+		{
+			ft_putchar(av[1][i] + 1);
+		}
+		else if(av[1][i] >= 'A' && av[1][i] <= 'Y')
+		{
+			ft_putchar(av[1][i] + 1);
+		}
+		else if(av[1][i] == 'z')
+		{
+			ft_putchar(av[1][i] - 25);
+		}
+		else if (av[1][i] == 'Z')
+		{
+			ft_putchar(av[1][i] - 25);
+		}
+		else
+		{
+			ft_putchar(av[1][i]);
+		}
+		i++;
+	}
+
+	write(1, "\n", 1);
+	return 0;
+}
