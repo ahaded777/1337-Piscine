27cbc1446a9a4f9d6da16aff4eba094faf83ada1
7NL <131011542+ahaded777@users.noreply.github.com>
2024-08-14 15:13:31 +0100

    Update ft_putnbr.c

--- days/C-Piscine-C00/ex07/ft_putnbr.c
+++ days/C-Piscine-C00/ex07/ft_putnbr.c
@@ -19,12 +19,7 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb)
 {
-	if (nb == -2147483648)
-	{
-		ft_putnbr(nb / 10);
-		ft_putchar('8');
-	}
-	else if (nb < 0)
+	if (nb < 0)
 	{
 		ft_putchar('-');
 		ft_putnbr(-nb);
